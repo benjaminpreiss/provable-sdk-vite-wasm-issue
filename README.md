697f16a24d8beeb3393e9a09af11bc0fc05a5340
Benjamin Preiss <benjaminpreiss@protonmail.com>
2025-04-10 00:03:19 +0200

    wip: setup provable sdk

---
+++ README.md
@@ -0,0 +1,7 @@
+# Steps to reproduction
+
+1. `npm i`
+2. `amareleo-chain start`
+3.  `npm run dev`
+
+Then go to `http://localhost:5173/` and check out the console, you will find multiple errors, e.g. `http://localhost:5173/node_modules/.vite/deps/aleo_wasm.wasm 404 (Not Found)`.
---
+++ src/lib/aleo.ts
@@ -0,0 +1,20 @@
+import {
+	Account,
+	AleoNetworkClient,
+	initializeWasm,
+	initThreadPool,
+	NetworkRecordProvider
+} from '@provablehq/sdk';
+
+export async function initClient() {
+	await initializeWasm();
+	// Enables multithreading
+	await initThreadPool();
+	const account = new Account({
+		privateKey: 'APrivateKey1zkp8CZNn3yeCseEtxuVPbDCwSyhGW6yZKUYKfgXmcpoGPWH.'
+	});
+	const networkClient = new AleoNetworkClient('http://0.0.0.0:3030');
+	networkClient.setAccount(account);
+	const recordProvider = new NetworkRecordProvider(account, networkClient);
+	return { account, networkClient, recordProvider };
+}
